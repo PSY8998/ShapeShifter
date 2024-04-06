f831be2f4140be6ec571ae7e052dc3349d7ce1d9
Parshant Yadav <parshantyadav954@gmail.com>
2024-04-06 11:19:26 +0530

    feat(activity): enabled edge to edge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity.compose)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
