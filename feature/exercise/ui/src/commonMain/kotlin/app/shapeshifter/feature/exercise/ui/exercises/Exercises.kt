ba9963eddc1020b69bce5f23ad25c03733629051
Parshant Yadav <parshantyadav954@gmail.com>
2024-04-24 10:32:30 +0530

    feat(exercise): Showed Exercise list from user to ui

--- feature/exercise/ui/src/commonMain/kotlin/app/shapeshifter/feature/exercise/ui/exercises/Exercises.kt
+++ feature/exercise/ui/src/commonMain/kotlin/app/shapeshifter/feature/exercise/ui/exercises/Exercises.kt
@@ -1,8 +1,13 @@
 package app.shapeshifter.feature.exercise.ui.exercises
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,12 +17,14 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import app.shapeshifter.common.ui.compose.NestedScaffold
 import app.shapeshifter.common.ui.compose.screens.ExercisesScreen
@@ -27,6 +34,10 @@ import com.slack.circuit.runtime.screen.Screen
 import com.slack.circuit.runtime.ui.Ui
 import com.slack.circuit.runtime.ui.ui
 import me.tatarka.inject.annotations.Inject
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+import shapeshifter.feature.exercise.ui.generated.resources.Res
+import shapeshifter.feature.exercise.ui.generated.resources.exercise_deadlift
 
 @Inject
 class ExercisesUiFactory : Ui.Factory {
@@ -91,7 +102,7 @@ private fun ExercisesContent(
         ExerciseScrollContent(
             modifier = Modifier
                 .fillMaxSize(),
-            exercises = state.exercises
+            exercises = state.exercises,
         )
 
         Button(
@@ -99,28 +110,49 @@ private fun ExercisesContent(
                 openCreateExercise()
             },
             modifier = Modifier
-                .align(Alignment.Center),
+                .align(Alignment.BottomCenter),
         ) {
             Text("Create Exercise")
         }
     }
 }
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 private fun ExerciseScrollContent(
     modifier: Modifier = Modifier,
-    exercises: List<Exercise>
+    exercises: List<Exercise>,
 ) {
     LazyColumn(
-        modifier = modifier
+        modifier = modifier,
     ) {
         items(exercises) {
-            Card(
-                modifier = Modifier,
-                shape = RectangleShape,
-                elevation = CardDefaults.cardElevation(10.dp)
-            ){
-                Text(it.name)
+            ElevatedCard(
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp,
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(8.dp),
+            ) {
+                Row(
+                    modifier = Modifier,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(Res.drawable.exercise_deadlift),
+                        contentDescription = "exercise image",
+                        modifier = Modifier,
+                    )
+
+                    Text(
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp),
+                        text = it.name,
+                        textAlign = TextAlign.End,
+                    )
+                }
             }
         }
     }
