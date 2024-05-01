6460fefad090ae35d0565e5a2d9cb4c418e7a801
Parshant Yadav <parshantyadav954@gmail.com>
2024-05-01 12:38:40 +0530

    feat(workout): TrackWorkout screen created

--- feature/workout/ui/src/commonMain/kotlin/app/shapeshifter/feature/workout/ui/trackworkout/TrackWorkout.kt
+++ feature/workout/ui/src/commonMain/kotlin/app/shapeshifter/feature/workout/ui/trackworkout/TrackWorkout.kt
@@ -1,27 +1,154 @@
 package app.shapeshifter.feature.workout.ui.trackworkout
 
+import android.graphics.Paint.Align
+import android.graphics.drawable.shapes.Shape
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import app.shapeshifter.common.ui.compose.screens.TrackWorkoutScreen
 import com.slack.circuit.runtime.CircuitContext
 import com.slack.circuit.runtime.screen.Screen
 import com.slack.circuit.runtime.ui.Ui
 import com.slack.circuit.runtime.ui.ui
 import me.tatarka.inject.annotations.Inject
+import java.util.Timer
 
 @Inject
 class TrackWorkoutUiFactory : Ui.Factory {
     override fun create(screen: Screen, context: CircuitContext): Ui<*>? {
         return when (screen) {
             is TrackWorkoutScreen -> ui<TrackWorkoutUiState> { state, modifier ->
-                TrackWorkout()
+                TrackWorkout(state)
             }
+
             else -> null
         }
     }
 }
 
 @Composable
-private fun TrackWorkout(){
-    Text("Workout in progress")
+private fun TrackWorkout(
+    state: TrackWorkoutUiState,
+) {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
+        ) {
+            TrackWorkoutTopBar(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onBack = {
+                    state.eventSink(TrackWorkoutUiEvent.GoBack)
+                },
+            )
+
+            Divider(thickness = 2.dp)
+
+            Spacer(
+                modifier = Modifier
+                    .padding(vertical = 8.dp),
+            )
+
+            ExerciseLog(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onAddExercise = {
+                    state.eventSink(TrackWorkoutUiEvent.OnAddExercise)
+                },
+            )
+        }
+    }
+}
+
+@Composable
+private fun TrackWorkoutTopBar(
+    modifier: Modifier = Modifier,
+    onBack: () -> Unit,
+    contentPadding: PaddingValues = PaddingValues(),
+) {
+    Row(
+        modifier = modifier
+            .padding(contentPadding),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        IconButton(
+            onClick = {
+                onBack()
+            },
+            modifier = Modifier,
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Default.KeyboardArrowLeft,
+                modifier = Modifier
+                    .size(32.dp),
+                contentDescription = "",
+            )
+        }
+
+        Text(
+            "Track Workout",
+            modifier = Modifier
+                .weight(1F),
+        )
+
+        Button(
+            onClick = {
+                onBack()
+            },
+            shape = CutCornerShape(
+                topStart = 16.dp,
+                topEnd = 0.dp,
+                bottomEnd = 0.dp,
+                bottomStart = 0.dp,
+            ),
+        ) {
+            Text("Finish")
+        }
+    }
+}
+
+@Composable
+private fun ExerciseLog(
+    modifier: Modifier = Modifier,
+    onAddExercise: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth(),
+    ) {
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(),
+            onClick = {
+                onAddExercise()
+            },
+        ) {
+            Text("+ Add Exercise")
+        }
+    }
 }
--- feature/workout/ui/src/commonMain/kotlin/app/shapeshifter/feature/workout/ui/trackworkout/TrackWorkoutPresenter.kt
+++ feature/workout/ui/src/commonMain/kotlin/app/shapeshifter/feature/workout/ui/trackworkout/TrackWorkoutPresenter.kt
@@ -2,17 +2,19 @@ package app.shapeshifter.feature.workout.ui.trackworkout
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import app.shapeshifter.common.ui.compose.screens.ExercisesScreen
 import app.shapeshifter.common.ui.compose.screens.TrackWorkoutScreen
 import app.shapeshifter.feature.workout.ui.savedworkouts.SavedWorkoutsPresenter
 import com.slack.circuit.runtime.CircuitContext
 import com.slack.circuit.runtime.Navigator
 import com.slack.circuit.runtime.presenter.Presenter
 import com.slack.circuit.runtime.screen.Screen
+import me.tatarka.inject.annotations.Assisted
 import me.tatarka.inject.annotations.Inject
 
 @Inject
 class TrackWorkoutPresenterFactory(
-    private val provideFactory: () -> TrackWorkoutPresenter,
+    private val provideFactory: (Navigator) -> TrackWorkoutPresenter,
 ) : Presenter.Factory {
     override fun create(
         screen: Screen,
@@ -20,17 +22,27 @@ class TrackWorkoutPresenterFactory(
         context: CircuitContext,
     ): Presenter<*>? {
         return when (screen) {
-            is TrackWorkoutScreen -> provideFactory()
+            is TrackWorkoutScreen -> provideFactory(navigator)
             else -> null
         }
     }
 }
 
 @Inject
-class TrackWorkoutPresenter : Presenter<TrackWorkoutUiState> {
+class TrackWorkoutPresenter(
+    @Assisted private val navigator: Navigator
+) : Presenter<TrackWorkoutUiState> {
 
     @Composable
     override fun present(): TrackWorkoutUiState {
-        return TrackWorkoutUiState
+        fun eventSink(event: TrackWorkoutUiEvent){
+            when(event){
+                is TrackWorkoutUiEvent.GoBack -> navigator.pop()
+                is TrackWorkoutUiEvent.OnAddExercise -> navigator.goTo(ExercisesScreen)
+            }
+        }
+        return TrackWorkoutUiState(
+            eventSink = :: eventSink
+        )
     }
 }
